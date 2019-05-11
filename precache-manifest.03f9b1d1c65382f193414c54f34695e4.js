70593a4a23a3fde7355537db9d878515293c7b15
Aleksey Bykhun <theisease@gmail.com>
2019-05-11 18:09:28 +0300

    Updates

---
+++ precache-manifest.03f9b1d1c65382f193414c54f34695e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf9ae4bc48dbd67e135f899d4a8c741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f71549d9e7aaa6514d0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "118415d394b8dac461e0",
+    "url": "/static/js/2.10a6e4ff.chunk.js"
+  },
+  {
+    "revision": "1f71549d9e7aaa6514d0",
+    "url": "/static/js/main.a0e1e829.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
