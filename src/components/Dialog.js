2395590ada5f9f39a35ca5808240cbff2160cf4b
Aleksey Bykhun <theisease@gmail.com>
2019-05-27 20:13:44 +0300

    print media links if available

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { ChatFeed, Message } from 'react-chat-ui'
 
+const convertTextURL = text => {
+  const [ _, head, protocol, url, tail ] = text.match(/^(.*)(https:\/\/)(.*?)(\s.*)?$/)
+
+  return (
+    <span>
+      {head}
+      <URL url={`https://${url}`} />
+      {tail}
+    </span>
+  )
+}
+
+const URL = props => <a target="_blank" rel="noopener noreferrer" href={props.url} {...props}>{props.text || props.url}</a>
+
+const getURL = {
+  media: media => <URL url={`https://instagram.com/p/${media.code}`} />,
+}
+
+const formatMessage = {
+  media: item => {
+    const image_urls = ((item.media.image_versions2 || {}).candidates || []).map(item => item.url)
+    const video_urls = (item.media.video_versions || []).map(item => item.url)
+
+    return (<Fragment>
+      {`raven_media`}<br />
+      {image_urls.map((item, index) => <span key={index}><URL url={item} text='Download Image' /><br /></span>)}
+      {video_urls.map((item, index) => <span key={index}><URL url={item} text='Download Video' /><br /></span>)}
+    </Fragment>)
+  },
+  media_share: item => formatMessage.media(item.media_share),
+  raven_media: item => {
+    const image_urls = ((item.raven_media.image_versions2 || {}).candidates || []).map(item => item.url)
+    const video_urls = (item.raven_media.video_versions || []).map(item => item.url)
+
+    return (<Fragment>
+      {`raven_media`}<br />
+      {image_urls.map((item, index) => <span key={index}><URL url={item} text='Download Image' /><br /></span>)}
+      {video_urls.map((item, index) => <span key={index}><URL url={item} text='Download Video' /><br /></span>)}
+    </Fragment>)
+  },
+  story_share: item => `story_share`,
+  reel_share: item => `reel_share ${item.reel_share.type}: ${item.reel_share.text}`,
+
+}
 
 export default class Dialog extends Component {
   state = {
@@ -11,7 +55,15 @@ export default class Dialog extends Component {
   componentWillReceiveProps(nextProps) {
     const messages = nextProps.messages.map(item => new Message({
       id: item.user_id === this.props.me ? 0 : item.user_id,
-      message: item.text || item.item_type,
+      message: item.item_type === 'text'
+        ? item.text
+        : item.item_type === 'media_share'
+        ? formatMessage.media_share(item)
+        : item.item_type === 'raven_media'
+        ? formatMessage.raven_media(item)
+        : formatMessage[item.item_type]
+        ? formatMessage[item.item_type](item)
+        : item[item.item_type].text || item[item.item_type].title || item[item.item_type].type || item.item_type,
     }))
 
     console.log('me', this.props.me)
